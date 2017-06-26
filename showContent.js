d23365c899c4d01940d07320f1afbd308dc782c9
Velestus <b.borawski92@gmail.com>
2017-06-26 20:33:30 +0200

    My CV page added
    Replaced contents of index.html with what I have written before.
    Added required scripts and styles.

---
+++ showContent.js
@@ -0,0 +1,23 @@
+function showContent(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
+
+function showNav() {
+	var x = document.getElementById("navbar");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+    var x = document.getElementById("demo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else { 
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
